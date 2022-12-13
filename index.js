6e0663caa942d3ca52d6970bec506041998d0acd
Lucas dias <lucasdiass061@gmail.com>
2022-12-13 15:11:55 -0300

    avanço 1.1

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-const app = express()
+const app = express();
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine','handlebars')
+app.engine('handlebars', exphbs.engine());
+app.set('view engine','handlebars');
 
 app.get('/', (req,res) => {
 
-    const autenticate = true
+    const autenticate = true;
 
-    res.render('home', {autenticate})
-})
+    const usuarios = ["Lucas", "Rykelmy", "Állan, "];
+
+    res.render('home', {autenticate, usuarios});
+});
 
 app.get('/info', (req,res) => {
     
     const user = {
         Nome: "Lucas",
         Idade: "18",
-        Cidade: "Campo Limpo Paulista"
-    }
+        Cidade: "Campo Limpo Paulista",
+        Nota: "10"
+    };
     
-    const mensagem = "sua infomação"
+    const aprovade = false;
+
+    const mensagem = "sua infomação";
 
 
-    res.render('info', {user: user, mensagem})
-})
+    res.render('info', {user: user, mensagem, aprovade});
+});
+
+app.get('/materia', (req,res) => {
+    const materia = {
+        Matematica: 8,
+        Portugues: 7,
+        Programação: 14,
+        Comentarios: 5
+    }
+    res.render('materia',{materia})
+});
 
 app.listen(3000, () =>{
-    console.log('App funcionante')
-})
+    console.log('App funcionante');
+});
